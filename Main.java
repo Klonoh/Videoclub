01242be5236fe3c008b055867ab55821d5571b5c
Klonoh <toquin2003@gmail.com>
2026-09-15 21:05:05 -0300

    Se agregan metodos a la vista de consola para mostrar la informacion de los clientes y peliculas

--- Main.java
+++ Main.java
@@ -3,8 +3,6 @@ public class Main {
     public static void main(String[] args) {
 
         VideoClub videoClub = new VideoClub();
-
-        System.out.println("Sistema de VideoClub iniciado.");
         VistaConsola vista = new VistaConsola(videoClub);
         vista.iniciar();
     }
